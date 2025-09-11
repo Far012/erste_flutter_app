c012e9367b24bec7bf83896f38f0a23d29b48940
Farhad <farhad.ahmadi@outlook.de>
2025-09-11 17:20:24 +0200

    update

--- lib/4.3.1_Stateless_Widgets/stateless_widgets.dart
+++ lib/4.3.1_Stateless_Widgets/stateless_widgets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Helloworld extends StatelessWidget {
-  const Helloworld({super.key});
+class Test extends StatelessWidget {
+  const Test({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:erste_flutter_app/P13_HelloWorldFlutterApp/hello_world.dart';
+import 'package:erste_flutter_app/4.2.4_P13_HelloWorldFlutterApp/hello_world.dart';
 import 'package:flutter/material.dart';
 
 void main() {
